624674f06a91821fc51063b039f26afe24e81bb5
KevinAndersonXactly <kanderson@xactlycorp.com>
2019-05-21 09:25:29 -0600

    added comments/notes

--- src/__tests__/react-testing-library.js
+++ src/__tests__/react-testing-library.js
@@ -1,4 +1,9 @@
 import 'jest-dom/extend-expect'
+//* this will auto cleanup after each test
+import 'react-testing-library/cleanup-after-each'
+
+// * jest has setup script framework file that you can put both above
+// * imports so you DONT have to import in each file anymore
 import React from 'react'
 // 🐨 remove this import:
 //import ReactDOM from 'react-dom'
@@ -16,9 +21,19 @@ test('renders a number input with a label "Favorite Number"', () => {
   //ReactDOM.render(<FavoriteNumber />, div)
   // 🐨 you don't need this anymore, you'll get the getByLabelText function from calling `render`
   //const {getByLabelText} = getQueriesForElement(div)
+
+  // * render() method is synonymous with Enzyme's 'mount' method
+
+  // * follows along with ethos that Shallow rendering is bad practice
+  // * getByLabelText will look through entire document.body
+  //const {getByLabelText, debug, fire, fireEvent, wait} = render(<FavoriteNumber />)
   const {getByLabelText} = render(<FavoriteNumber />)
   const input = getByLabelText(/favorite number/i)
   expect(input).toHaveAttribute('type', 'number')
+  // * debug(will console.log(document.body))
+  // * debug()
+  // * dubug(input)  will console.log the node
+  // * debug(container) will console.log the container
 })
 
 //////// Elaboration & Feedback /////////
